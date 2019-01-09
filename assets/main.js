02d254f35c30005d44dd14fe4406a7885bbf3be0
aaryan10 <aaryanbudhiraja2@gmail.com>
2019-01-09 17:42:19 +0530

    Added one obstacle

--- assets/main.js
+++ assets/main.js
@@ -3,16 +3,21 @@ let gravity = -10;
 let obstacleSpeed = -10;
 let birdSize = 50;
 let bird = null;
+let obstacle = null;
+let obstacleWidth = 75;
+let passingHeight = 3*birdSize; 
 
 function setup () {
     createCanvas(800, 600);
     background(color(135,206,250));
     bird = new Bird();
-    
+    obstacle = new Obstacle(400,200);
 }
 
 function draw () {
     bird.show();
+    obstacle.show();
+    // rect(400, 0, 75, 200);
 }
 
 class Bird {
@@ -28,4 +33,18 @@ class Bird {
     show() {
         ellipse(this.x, this.y, birdSize, birdSize);
     }
+}
+
+class Obstacle {
+    constructor(xPosition, height) {
+        this.xPosition = xPosition;
+        this.height = height;
+    }
+    show() {
+        // translate(-50, 0);
+        rect(this.xPosition, 0, obstacleWidth, this.height);
+    }
+    // update()    {
+    //     // translate(30, 20);
+    // }
 }
