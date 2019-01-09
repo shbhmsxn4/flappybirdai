2d3abd799b32c86e581c4ba11b2ff8d057cf2d09
Shubham Saxena <shbhmsxn4@gmail.com>
2019-01-09 16:51:08 +0530

    Added bird class.

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,31 @@
+let flapPower = 5;
+let gravity = -10;
+let obstacleSpeed = -10;
+let birdSize = 50;
+let bird = null;
+
 function setup () {
     createCanvas(800, 600);
     background(color(135,206,250));
+    bird = new Bird();
+    
 }
 
 function draw () {
-    ellipse(50,50,50,50);
+    bird.show();
+}
+
+class Bird {
+    constructor() {
+        this.x = 100;
+        this.y = 300;g
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+    }
+    flap() {
+        this.yVelocity = flapPower;
+    }
+    show() {
+        ellipse(this.x, this.y, birdSize, birdSize);
+    }
 }
