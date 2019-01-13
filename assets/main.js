6ca50d5a8cbb896b7dd779371bcff1ea3b5d1d96
Shubham Saxena <shbhmsxn4@gmail.com>
2019-01-13 20:39:57 +0530

    Fixed mutate funtion. Tweaked hyperparameters.

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,18 @@
-//AI CONTROL VARIABLES
+// --------------------AI Parameters--------------------
 
 let networkLayers = [4, 1];
 
 let crossoverUniformity = 0.5;
-let mutationProbability = 0.01;
-let tournamentSize = 5;
-let elitism = true;
-
+let mutationProbability = 0.8;
+let mutationProbabilityIncrement = 0.05;
+let maxMutationProbability = 0.95;
+let tournamentSize = 50;
+let elitism = false;
+let randomIndividuals = 0;
 let currentGeneration;
-let generationSize = 10;
+let generationSize = 500;
 
-//GAME CONTROL VARAIBLES
+// --------------------Game Parameters--------------------
 
 let flapPower = -5;
 let gravity = 0.25;
@@ -21,12 +23,14 @@ let obstacleWidth = 75;
 let passingHeight = 3*birdSize; 
 let obstacleDistance = 3.8 * obstacleWidth;
 
-//AI VARIABLES
+// --------------------AI Variables--------------------
+
+let highscore = 0;
 let currentBirds = [];
 let ga;
 let undeadPlayers;
 
-//LOADING RESOURCES
+// --------------------Resource Variables--------------------
 
 let img;
 let bg;
@@ -36,112 +40,11 @@ let font;
 let fontSize = 40;
 let fontPath = "assets/fonts/theboldfont.ttf";
 
-//TRASH VARIABLES
-
-// let bird = null;
-// let gameOver = null;
-// let score = 0;
-// let birdInPipe = false;
-// let birdWasInPipe = false;
-
 
 function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
 }
-
-// function setup () {
-//     createCanvas(1000, 600);
-//     background(color(135,206,250));
-//     img = loadImage("assets/images/bird.png");  // Load the image
-//     // img = loadImage("assets/images/squarebird.png");
-//     bg = loadImage("assets/images/bg.png");
-//     pipel = loadImage("assets/images/pipe-l.png");
-//     pipeu = loadImage("assets/images/pipe-u.png");
-//     font = loadFont(fontPath);
-//     textFont(font);
-//     textSize(fontSize);
-//     textAlign(CENTER, CENTER);
-//     // bird = new Bird();
-//     setupBirds();
-//     birds = currentBirds;
-//     obstacle = [];
-//     obstacle.push(new Obstacle(400,randomInteger(75, height - passingHeight-75)));
-//     obstacle.push(new Obstacle(400 + obstacleDistance,randomInteger(75, height - passingHeight-75)));
-//     obstacle.push(new Obstacle(400 + 2* obstacleDistance,randomInteger(75, height - passingHeight-75)));
-//     obstacle.push(new Obstacle(400 + 3* obstacleDistance,randomInteger(75, height - passingHeight-75)));
-//     obstacle.push(new Obstacle(400 + 4* obstacleDistance,randomInteger(75, height - passingHeight-75)));
-//     obstacle.push(new Obstacle(400 + 5* obstacleDistance,randomInteger(75, height - passingHeight-75)));
-//     // gameOver = false;
-//     // score = 0;
-//     // birdInPipe = false;
-//     // birdWasInPipe = false;
-// }
-
-// function draw () {
-//     if (birds.length != 0) {
-//         // background(color(135,26,250));
-//         background(bg);
-//         birds.forEach(b => {
-//             b.update();
-//             b.show();
-//         });
-//         // bird.update();
-//         // bird.show();
-//         obstacle.forEach(o => {
-//             o.update();
-//             o.show();
-//         });
-//         fill(255);
-//         showScore();
-//         // birdInPipe = false;
-//         obstacle.forEach(o =>    {
-//             // if( (bird.top < o.upperHeight || bird.bottom > o.lowerHeight) && (bird.right > o.left && bird.left < o.right) ){
-//             //     gameOver = true;
-//             // }
-//             // if (bird.right > o.left && bird.left < o.right) {
-//             //     birdInPipe = true;
-//             // }
-//             birds.forEach(b => {
-//                 if( (b.top < o.upperHeight || b.bottom > o.lowerHeight) && (b.right > o.left && b.left < o.right) ){
-//                     b.gameOver = true;
-//                 }
-//                 if (b.right > o.left && b.left < o.right) {
-//                     b.isInPipe = true;
-//                 }
-//             });
-//         });
-//         // if (birdWasInPipe && !birdInPipe) {
-//         //     score++;
-//         //     birdWasInPipe = false;
-//         // }
-//         // else {
-//         //     birdWasInPipe = birdInPipe;
-//         // }
-//         birds.forEach(b => {
-//             if (b.wasInPipe && !b.isInPipe) {
-//                 b.score++;
-//                 b.wasInPipe = false;
-//             }
-//             else {
-//                 b.wasInPipe = b.isInPipe;
-//                 b.isInPipe = false;
-//             }
-//         });
-//         if(obstacle[0].xPosition <= -obstacleWidth){
-//             obstacle.shift();
-//             obstacle.push( new Obstacle(obstacle[obstacle.length-1].xPosition + obstacleDistance, randomInteger(75, height - passingHeight-75)) );
-//         }
-//         birds = birds.filter(function (b) {
-//             return b.gameOver == false;
-//         });
-//     }
-//     else {
-//         if (keyIsPressed) {
-//             setup();
-//         }
-//     }
-// }
 
 function setup () {
     createCanvas(1000, 600);
@@ -168,10 +71,6 @@ function setup () {
     obstacle.push(new Obstacle(400 + 3* obstacleDistance,randomInteger(75, height - passingHeight-75)));
     obstacle.push(new Obstacle(400 + 4* obstacleDistance,randomInteger(75, height - passingHeight-75)));
     obstacle.push(new Obstacle(400 + 5* obstacleDistance,randomInteger(75, height - passingHeight-75)));
-    // gameOver = false;
-    // score = 0;
-    // birdInPipe = false;
-    // birdWasInPipe = false;
 }
 
 function draw () {
@@ -189,14 +88,7 @@ function draw () {
         });
         fill(255);
         showScore();
-        // birdInPipe = false;
         obstacle.forEach(o =>    {
-            // if( (bird.top < o.upperHeight || bird.bottom > o.lowerHeight) && (bird.right > o.left && bird.left < o.right) ){
-            //     gameOver = true;
-            // }
-            // if (bird.right > o.left && bird.left < o.right) {
-            //     birdInPipe = true;
-            // }
             undeadPlayers.forEach(p => {
                 if( (p.bird.top < o.upperHeight || p.bird.bottom > o.lowerHeight) && (p.bird.right > o.left && p.bird.left < o.right) ){
                     p.bird.gameOver = true;
@@ -206,13 +98,6 @@ function draw () {
                 }
             });
         });
-        // if (birdWasInPipe && !birdInPipe) {
-        //     score++;
-        //     birdWasInPipe = false;
-        // }
-        // else {
-        //     birdWasInPipe = birdInPipe;
-        // }
         undeadPlayers.forEach(p => {
             if (p.bird.wasInPipe && !p.bird.isInPipe) {
                 p.bird.score++;
@@ -232,43 +117,34 @@ function draw () {
         });
     }
     else {
-        // if (keyIsPressed) {
-        //     setup();
-        // }
         ga.formNextGeneration();
         setup();
     }
 }
 
-// function setupBirds() {
-//     currentBirds = [];
-//     currentBirds.push(new Bird(100,300));
-//     currentBirds.push(new Bird(100,200));
-// }
-
 function showScore() {
-    // text("Score: " + score.toString(), width/2, 40);
     let maxScore = 0;
     undeadPlayers.forEach(p => {
         if (p.bird.score > maxScore) {
             maxScore = p.bird.score;
         }
     });
+    highscore = math.max(highscore, maxScore);
     text("Score: " + maxScore.toString(), width/2, 40);
     text("Generation: " + currentGeneration.generationNumber.toString(), width/2, 100);
+    text("Record: " + highscore.toString(), width/2, 160);
 }
 
-function keyPressed() {
-    switch(keyCode) {
-        case 32:
-            // bird.flap();
-            birds.forEach(b => {
-                b.flap();
-            });
-            break;
-    }
-    return false;
-}
+// function keyPressed() {
+//     switch(keyCode) {
+//         case 32:
+//             birds.forEach(b => {
+//                 b.flap();
+//             });
+//             break;
+//     }
+//     return false;
+// }
 
 class Bird {
     constructor(x, y) {
@@ -313,9 +189,7 @@ class Obstacle {
     }
     show() {
         fill(5,135,56);
-        // rect(this.left, this.upperHeight, obstacleWidth, passingHeight-height);
         image(pipeu,this.left, this.upperHeight, obstacleWidth, passingHeight-height );
-        // rect(this.left, this.lowerHeight, obstacleWidth, height - passingHeight);
         image(pipel,this.left, this.lowerHeight, obstacleWidth, height - passingHeight );
     }
     update()    {
@@ -442,6 +316,9 @@ class GeneticAlgorithm {
         return [newWeights, newBiases];
     }
     formNextGeneration() {
+        // generationSize = math.max(10, generationSize/2);
+        let randomIndividualsNeeded = randomIndividuals;
+        // mutationProbability = math.min(maxMutationProbability, mutationProbability + mutationProbabilityIncrement);
         let nextGeneration = new Generation(generationSize, false);
         let loopCounter = generationSize;
         if (elitism) {
@@ -456,6 +333,11 @@ class GeneticAlgorithm {
             }
             nextGeneration.individuals[generationSize-1].network.setParameters( [currentGeneration.individuals[maxFitnessIndex].network.weights, currentGeneration.individuals[maxFitnessIndex].network.biases] );
             loopCounter--;
+        }
+        while (randomIndividualsNeeded > 0) {
+            nextGeneration.individuals[loopCounter - 1].network.generateParameters();
+            loopCounter--;
+            randomIndividualsNeeded--;
         }
         for (let i=0 ; i<loopCounter ; i++) {
             nextGeneration.individuals[i].network.setParameters( this.conductTournament() );
@@ -506,11 +388,6 @@ class AIPlayer {
     takeTurn() {
         this.updateInputs();
         this.takeFlapDecision();
-        // if (this.bird.isInPipe) {
-        //     this.fitness = this.bird.score + 0.5;
-        // } else {
-        //     this.fitness = this.bird.score;
-        // }
         this.fitness++;
     }
     updateInputs() {
@@ -521,10 +398,10 @@ class AIPlayer {
                 break;
             }
         }
-        this.inputs[0] = (nextObstacle.xPosition - this.bird.right)/obstacleDistance;       //x-distance to next pipe
-        this.inputs[1] = (nextObstacle.upperHeight - this.bird.top)/height;       //y-distance to top of pipe
-        this.inputs[2] = (nextObstacle.lowerHeight - this.bird.bottom)/height;    //y-distance to bottom of pipe
-        this.inputs[3] = this.bird.yVelocity/20;
+        this.inputs[0] = (2*(nextObstacle.xPosition - this.bird.right)/obstacleDistance) - 1;       //x-distance to next pipe
+        this.inputs[1] = (2*(nextObstacle.upperHeight - this.bird.top)/height) - 1;                 //y-distance to top of pipe
+        this.inputs[2] = (2*(nextObstacle.lowerHeight - this.bird.bottom)/height) - 1;              //y-distance to bottom of pipe
+        this.inputs[3] = (2*this.bird.yVelocity/(math.sqrt(2*gravity*height))) - 1;
         this.inputVector = math.matrix(this.inputs);
     }
     takeFlapDecision() {
@@ -535,147 +412,24 @@ class AIPlayer {
     mutate() {
         let newWeights = math.clone(this.network.weights);
         let newBiases = math.clone(this.network.biases);
-        math.map(newWeights, function(value) {
-            if (math.random() < mutationProbability) {
-                return math.random(-1, 1);
-            } else {
-                return value;
-            }
+        newWeights.forEach(w => {
+            w = math.map(w, function(value) {
+                if (math.random() < mutationProbability) {
+                    return math.random(-1, 1);
+                } else {
+                    return value;
+                }
+            });
+        });
+        newBiases.forEach(b => {
+            b = math.map(b, function(value) {
+                if (math.random() < mutationProbability) {
+                    return math.random(-1, 1);
+                } else {
+                    return value;
+                }
+            });
         });
         return [newWeights, newBiases];
     }
 }
-
-
-
-//--------------------TRASH--------------------
-
-// //--------------------ARTIFICIAL INTELLIGENCE--------------------
-
-// // Inputs: 
-// //      - distance to next pipe
-// //      - distance to top of next pipe
-// //      - distance to bottom of next pipe
-// //      - y velocity of bird
-
-// // Output:
-// //      - flap or not
-
-// let networkLayers = [4, 2, 1];
-
-// //--------------------NEURAL NETWORK--------------------
-
-// class Network {
-//     constructor(layerSizes) {
-//         this.numLayers = layerSizes.length;
-//         this.layerSizes = layerSizes;
-//         this.biases = [];
-//         this.weights = []; 
-//     }
-//     generateParameters() {
-//         for (let i in layerSizes) {
-//             if (i) {
-//                 this.biases.push(tf.variable(tf.randomNormal([this.layerSizes[i], 1])));
-//                 this.weights.push(tf.variable(tf.randomNormal([this.layerSizes[i], this.layerSizes[i-1]])));
-//             }
-//         }
-//     }
-//     setWeights(weights) {
-//         this.weights = weights;
-//     }
-//     feedforward(inputs) {
-//         let activations = inputs;
-//         for (let i in this.biases) {
-//             activations = tf.relu(tf.add(tf.dot(this.weights[i], activations), this.biases[i]));
-//         }
-//         return activations;
-//     }
-// }
-
-
-// //--------------------GENETIC ALGORITHM--------------------
-
-// let crossoverUniformity = 0.5;
-// let mutationProbability = 0.01;
-// let tournamentSize = 5;
-// let elitism = true;
-
-// class Individual {
-//     constructor() {
-//         this.bird = new Bird();
-//         this.network = new Network(networkLayers);
-//     }
-
-// }
-
-// class Generation {
-//     constructor(population, createRandom) {
-//         this.population = population;
-//         this.individuals = [];
-//         if (createRandom) {
-//             for (let i=0 ; i<population ; i++) {
-//                 let individual = new Individual();
-//                 individual.network.generateParameters();
-//                 individuals.push(individual);
-//             }
-//         } else {
-//             for (let i=0 ; i<population ; i++) {
-//                 individuals.push(new Individual());
-//             }
-//         }
-//     }
-// }
-
-// class GeneticAlgorithm {
-//     constructor() {
-//         this.generationNumber = null;
-//     }
-//     crossover(player1, player2) {
-
-//     }
-
-//     runCurrentGeneration() {
-
-//     }
-// }
-
-// class AIPlayer {
-//     constructor() {
-//         this.bird = new Bird();
-//         this.network = new Network(networkLayers);
-//         this.inputs = [0, 0, 0, 0];
-//         this.inputTensor = null;
-//     }
-//     takeTurn() {
-//         updateInputs();
-//         takeFlapDecision();
-//     }
-//     updateInputs() {
-//         let nextObstacle = null;
-//         for (let i=0 ; i<10 ; i++) {
-//             if ( !( (obstacle[i].xPosition + obstacleWidth) < this.bird.right ) ) {
-//                 nextObstacle = obstacle[i];
-//                 break;
-//             }
-//         }
-//         inputs[0] = nextObstacle.xPosition - this.bird.right;       //x-distance to next pipe
-//         inputs[1] = nextObstacle.upperHeight - this.bird.top;       //y-distance to top of pipe
-//         inputs[2] = nextObstacle.lowerHeight - this.bird.bottom;    //y-distance to bottom of pipe
-//         inputs[3] = this.bird.yVelocity;
-//         this.inputTensor = tf.tensor1d(inputs);
-//     }
-//     takeFlapDecision() {
-//         if (this.network.feedforward(this.inputTensor).toBool()) {
-//             this.bird.flap();
-//         }
-//     }
-//     mutate() {
-//         let newWeights = [];
-//         let newBiases = [];
-//         for (let i in this.weights) {
-//             let w = tf.buffer(this.weights[i].shape);
-//             newWeights.push();
-//         }
-//         return [newWeights, newBiases];
-//     }
-// }
